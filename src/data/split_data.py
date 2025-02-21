eada84bc20ff0b88021f809f2dd75886b9ca5263
Marco <marco@datascientest.com>
2025-02-21 10:21:34 +0000

    Update dvc.yaml

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -12,11 +12,11 @@ y = df.iloc[:, -1]   # Last column (silica_concentrate)
 # Split the data into training and test sets 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-os.makedirs("data/processed_data", exist_ok=True)
+os.makedirs("data/processed", exist_ok=True)
 
 # Save the datasets
-X_train.to_csv("data/processed_data/X_train.csv", index=False)
-X_test.to_csv("data/processed_data/X_test.csv", index=False)
-y_train.to_csv("data/processed_data/y_train.csv", index=False)
-y_test.to_csv("data/processed_data/y_test.csv", index=False)
+X_train.to_csv("data/processed/X_train.csv", index=False)
+X_test.to_csv("data/processed/X_test.csv", index=False)
+y_train.to_csv("data/processed/y_train.csv", index=False)
+y_test.to_csv("data/processed/y_test.csv", index=False)
 
